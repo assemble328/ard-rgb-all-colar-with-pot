202670ec88d8a8b44e0218d6299913106a48a136
assemble328 <77635553+assemble328@users.noreply.github.com>
2021-04-30 14:07:49 +0300

    Add files via upload

---
+++ rgb_e_i.ino
@@ -0,0 +1,633 @@
+const int PIN_RED   = 5;
+const int PIN_GREEN = 6;
+const int PIN_BLUE  = 9;
+int t;
+
+void setup() {
+  pinMode(A0,INPUT);   
+  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE,  OUTPUT);
+}
+
+void loop() {
+  
+  // Alice Mavi F0F8FF  240,248,255
+  setColor(240,248,255);
+  delay(t); // keep the color 1 second
+
+  //Antik Beyaz  FAEBD7  250,235,215
+  setColor(250,235,215);
+  delay(t); // keep the color 1 second
+
+  
+  //Aqua  00FFFF  0,255,255
+  setColor(0,255,255);
+  delay(t); // keep the color 1 second
+
+  
+  //Akuamarin  7FFFD4  127.255.212
+  setColor(127,255,212);
+  delay(t); // keep the color 1 second
+
+  
+  //Azure  F0FFFF  240.255.255
+  setColor(240,255,255);
+  delay(t); // keep the color 1 second
+
+  
+  //Bej  F5F5DC  245.245.220
+  setColor(245,245,220);
+  delay(t); // keep the color 1 second
+
+  
+  //Bisque  FFE4C4  255.228.196
+  setColor(255,228,196);
+  delay(t); // keep the color 1 second
+
+  
+  //BeyazlatılmışAlmond  FFEBCD  255.235.205
+  setColor(255,235,205);
+  delay(t); // keep the color 1 second
+
+  
+  //Mavi  0000FF  0,0,255
+  setColor(0,0,255);
+  delay(t); // keep the color 1 second
+
+  
+  //Mavi menekşe  8A2BE2  138,43,226
+  setColor(138,43,226);
+  delay(t); // keep the color 1 second
+
+  
+  //Kahverengi  A52A2A  165,42,42
+  setColor(165,42,42);
+  delay(t); // keep the color 1 second
+
+  
+  //BurlyWood  DEB887  222.184.135
+  setColor(222,184,135);
+  delay(t); // keep the color 1 second
+
+  //CadetBlue  5F9EA0  95.158.160
+  setColor(95,158,160);
+  delay(t); // keep the color 1 second
+
+  //Chartreuse  7FFF00  127.255,0
+  setColor(127,255,0);
+  delay(t); // keep the color 1 second
+
+  
+  //Çikolata  D2691E  210,105,30
+  setColor(210,105,30);
+  delay(t); // keep the color 1 second
+
+  
+  //Mercan  FF7F50  255,127,80
+  setColor( 255,127,80);
+  delay(t); // keep the color 1 second
+
+  
+  //Peygamber ÇiçeğiMavi  6495ED  100.149.237
+  setColor(100,149,237);
+  delay(t); // keep the color 1 second
+
+  
+  //Mısır püskülü  FFF8DC  255.248.220
+  setColor(255,248,220);
+  delay(t); // keep the color 1 second
+
+  
+  //Kızıl  DC143C  220,20,60
+  setColor(220,20,60);
+  delay(t); // keep the color 1 second
+
+  
+  //Camgöbeği  00FFFF  0,255,255
+  setColor( 0,255,255);
+  delay(t); // keep the color 1 second
+
+  
+  //DarkCyan  008B8B  0,139,139
+  setColor(0,139,139);
+  delay(t); // keep the color 1 second
+
+  
+  //DarkGoldenRod  B8860B  184,134,11
+  setColor(184,134,11);
+  delay(t); // keep the color 1 second
+
+  
+  //Koyu gri  A9A9A9  169.169.169
+  setColor(169,169,169);
+  delay(t); // keep the color 1 second
+
+  
+  //Koyu yeşil  006400  0,100,0
+  setColor( 0,100,0);
+  delay(t); // keep the color 1 second
+//
+ // Koyu Haki  BDB76B  189.183.107
+  setColor(189,183,107);
+  delay(t); // keep the color 1 second
+
+  //DarkMagenta  8B008B  139.0.139
+  setColor(139,0,139);
+  delay(t); // keep the color 1 second
+
+  
+  //DarkOliveGreen  556B2F  85.107.47
+  setColor(85,107,47);
+  delay(t); // keep the color 1 second
+
+  
+  //Koyu turuncu  FF8C00  255.140,0
+  setColor(255,140,0);
+  delay(t); // keep the color 1 second
+
+  
+  //DarkOrchid  9932CC  153,50,204
+  setColor(153,50,204);
+  delay(t); // keep the color 1 second
+
+  
+  //Koyu Kırmızı  8B0000  139,0,0
+  setColor(139,0,0);
+  delay(t); // keep the color 1 second
+
+  
+  //DarkSalmon  E9967A  233.150.122
+  setColor(233,150,122);
+  delay(t); // keep the color 1 second
+
+  
+  //DarkSeaGreen  8FBC8F  143.188.143
+  setColor(143,188,143);
+  delay(t); // keep the color 1 second
+
+  
+  //DarkSlateBlue  483D8B  72.61.139
+  setColor(72,61,139);
+  delay(t); // keep the color 1 second
+
+  
+  //DarkSlateGray  2F4F4F  47,79,79
+  setColor(47,79,79);
+  delay(t); // keep the color 1 second
+
+  
+  //KoyuTurkuaz  00CED1  0,206,209
+  setColor(0,206,209);
+  delay(t); // keep the color 1 second
+
+  
+  //Koyu mor  9400D3  148,0,211
+  setColor(148,0,211);
+  delay(t); // keep the color 1 second
+
+  //Derin pembe  FF1493  255,20,147
+  setColor(255,20,147);
+  delay(t); // keep the color 1 second
+
+  //DeepSkyBlue  00BFFF  0,191,255
+  setColor(0,191,255);
+  delay(t); // keep the color 1 second
+
+  
+  //DimGrey  696969  105.105.105
+  setColor(105,105,105);
+  delay(t); // keep the color 1 second
+
+  
+  //Mercan  FF7F50  255,127,80
+  setColor( 255,127,80);
+  delay(t); // keep the color 1 second
+
+  
+  //DodgerBlue  1E90FF  30.144.255
+  setColor(30,144,255);
+  delay(t); // keep the color 1 second
+
+  
+  //FireBrick  B22222  178,34,34
+  setColor(178,34,34);
+  delay(t); // keep the color 1 second
+
+  
+  //Çiçek Beyazı  FFFAF0  255.250.240
+  setColor(255,250,240);
+  delay(t); // keep the color 1 second
+
+  
+  //Orman yeşili  228B22  34.139.34
+  setColor(34,139,34);
+  delay(t); // keep the color 1 second
+
+  
+  //Fuşya  FF00FF  255,0.255
+  setColor(255,0,2559);
+  delay(t); // keep the color 1 second
+
+  
+  //Gainsboro  DCDCDC  220,220,220
+  setColor( 220,220,220);
+  delay(t); // keep the color 1 second
+
+  
+  //Hayalet Beyaz  F8F8FF  248.248.255
+  setColor(248,248,255);
+  delay(t); // keep the color 1 second
+
+  
+  //Altın  FFD700  255,215,0
+  setColor( 255,215,0);
+  delay(t); // keep the color 1 second
+
+  
+  //
+
+
+
+  // GoldenRod DAA520  218.165,32
+  setColor(218,165,32);
+  delay(t); // keep the color 1 second
+
+  //Gri  808080  128.128.128
+  setColor(128,128,128);
+  delay(t); // keep the color 1 second
+
+  
+  //Yeşil  008000  0,128,0
+  setColor(0,128,0);
+  delay(t); // keep the color 1 second
+
+  
+  //Yeşil sarı  ADFF2F  173,255,47
+  setColor(173,255,47);
+  delay(t); // keep the color 1 second
+
+  
+  //Şeker kavunu  F0FFF0  240.255.240
+  setColor(240,255,240);
+  delay(t); // keep the color 1 second
+
+  
+  //Sıcak pembe  FF69B4  255.105.180
+  setColor(255,105,180);
+  delay(t); // keep the color 1 second
+
+  
+  //Kızılderili  CD5C5C  205,92,92
+  setColor(205,92,92);
+  delay(t); // keep the color 1 second
+
+  
+  //Indigo  4B0082  75,0.130
+  setColor(75,0,130);
+  delay(t); // keep the color 1 second
+
+  
+  //Fildişi  FFFFF0  255.255.240
+  setColor(255,255,240);
+  delay(t); // keep the color 1 second
+
+  
+  //Haki  F0E68C  240,230,140
+  setColor(240,230,140);
+  delay(t); // keep the color 1 second
+
+  
+  //Lavanta  E6E6FA  230,230,250
+  setColor( 230,230,250);
+  delay(t); // keep the color 1 second
+
+  
+  //LavantaPlush  FFF0F5  255.240.245
+  setColor(255,240,245);
+  delay(t); // keep the color 1 second
+
+  //Çim Yeşil  7CFC00  124.252,0
+  setColor(124,252,0);
+  delay(t); // keep the color 1 second
+
+  //Limon şifon  FFFACD  255.250.205
+  setColor(255,250,205);
+  delay(t); // keep the color 1 second
+
+  
+  //Açık mavi  ADD8E6  173,216,230
+  setColor(173,216,230);
+  delay(t); // keep the color 1 second
+
+  
+  //LightCoral  F08080  240.128.128
+  setColor( 240,128,128);
+  delay(t); // keep the color 1 second
+
+  
+  //Açık cam göbeği  E0FFFF  224.255.255
+  setColor(224,255,255);
+  delay(t); // keep the color 1 second
+
+  
+  //LightGoldenRodSarı  FAFAD2  250,250,210
+  setColor(250,250,210);
+  delay(t); // keep the color 1 second
+
+  
+  //Açık gri  D3D3D3  211,211,211
+  setColor(211,211,211);
+  delay(t); // keep the color 1 second
+
+  
+  //Açık yeşil  90EE90  144.238.144
+  setColor( 144,238,144);
+  delay(t); // keep the color 1 second
+
+  
+  //Açık pembe  FFB6C1  255.182.193
+  setColor(255,182,193);
+  delay(t); // keep the color 1 second
+
+  
+  //Hafif Somon  FFA07A  255.160.122
+  setColor(255,160,122);
+  delay(t); // keep the color 1 second
+
+  
+  //LightSeaGreen  20B2AA  32.178.170
+  setColor(32,178,170);
+  delay(t); // keep the color 1 second
+
+  
+  //LightSkyBlue  87 CEFA 135.206.250
+  setColor(135,206,250);
+  delay(t); // keep the color 1 second
+//
+ // LightSlateGray  778899  119.136.153
+  setColor(119,136,153);
+  delay(t); // keep the color 1 second
+
+  //LightSteelBlue  B0C4DE  176.196.222
+  setColor(176,196,222);
+  delay(t); // keep the color 1 second
+
+  
+  //Açık sarı  FFFFE0  255.255.224
+  setColor(255,255,224);
+  delay(t); // keep the color 1 second
+
+  
+  //Kireç  00FF00  0,255,0
+  setColor(0,255,0);
+  delay(t); // keep the color 1 second
+
+  
+  //Limon yeşili  32CD32  50,205,50
+  setColor(50,205,50);
+  delay(t); // keep the color 1 second
+
+  
+  //Keten  FAF0E6  250,240,230
+  setColor(250,240,230);
+  delay(t); // keep the color 1 second
+
+  
+  //Eflatun  FF00FF  255,0.255
+  setColor(255,0,255);
+  delay(t); // keep the color 1 second
+
+  
+  //Bordo  800000  128,0,0
+  setColor( 128,0,0);
+  delay(t); // keep the color 1 second
+
+  
+  //OrtaAquaMarine  66CDAA  102.205.170
+  setColor( 102,205,170);
+  delay(t); // keep the color 1 second
+
+  
+  //Orta mavi  0000CD  0,0,205
+  setColor(0,0,205);
+  delay(t); // keep the color 1 second
+
+  
+  //OrtaOrchid  BA55D3  186,85,211
+  setColor( 186,85,211);
+  delay(t); // keep the color 1 second
+
+  
+  //OrtaMor  9370DB  147.112.219
+  setColor(147,112,219);
+  delay(t); // keep the color 1 second
+
+  //Orta Deniz Yeşil  3CB371  60.179.113
+  setColor(60,179,113);
+  delay(t); // keep the color 1 second
+
+  //MediumSlateBlue  7B68EE  123.104.238
+  setColor(123,104,238);
+  delay(t); // keep the color 1 second
+
+  
+  //OrtaBaharYeşil  00FA9A  0,250,154
+  setColor(0,250,154);
+  delay(t); // keep the color 1 second
+
+  
+  //OrtaTurkuaz  48D1CC  72.209.204
+  setColor( 72,209,204);
+  delay(t); // keep the color 1 second
+
+  
+  //Orta MenekşeKırmızı  C71585  199.21.133
+  setColor(199,21,133);
+  delay(t); // keep the color 1 second
+
+  
+  //Gece yarısı mavisi  191970  25,25,112
+  setColor(  25,25,112);
+  delay(t); // keep the color 1 second
+
+  
+  //MintCream  F5FFFA  245.255.250
+  setColor( 245,255,250);
+  delay(t); // keep the color 1 second
+
+  
+  //NavajoBeyaz  FFDEAD  255.222.173
+  setColor(255,222,173);
+  delay(t); // keep the color 1 second
+
+  
+  //zeytin  808000  128.128,0
+  setColor(128,128,0);
+  delay(t); // keep the color 1 second
+
+  
+  //Portakal  FFA500  255.165,0
+  setColor( 255,165,0);
+  delay(t); // keep the color 1 second
+
+  
+  //TuruncuKırmızı  FF4500  255,69,0
+  setColor(255,69,0);
+  delay(t); // keep the color 1 second
+
+  
+  //Orkide  DA70D6  218.112.214
+  setColor( 218,112,214);
+  delay(t); // keep the color 1 second
+
+  
+  //
+  //
+  //
+
+
+
+  //PaleGoldenRod  EEE8AA  238,232,170
+  setColor(238,232,170);
+  delay(t); // keep the color 1 second
+
+  //Soluk yeşil  98FB98  152.251.152
+  setColor(152,251,152);
+  delay(t); // keep the color 1 second
+
+  
+  //Açık Turkuaz  AFEEEE  175.238.238
+  setColor(175,238,238);
+  delay(t); // keep the color 1 second
+
+  
+  //PaleVioletRed  DB7093  219.112.147
+  setColor( 219,112,147);
+  delay(t); // keep the color 1 second
+
+  
+  //PeachPuff  FFDAB9  255.218.185
+  setColor(255,218,185);
+  delay(t); // keep the color 1 second
+
+  
+  //Erik  DDA0DD  221.160.221
+  setColor(221,160,221);
+  delay(t); // keep the color 1 second
+
+  
+  //Toz mavi  B0E0E6  176.224.230
+  setColor(176,224,230);
+  delay(t); // keep the color 1 second
+
+  
+  //Mor  800080  128,0.128
+  setColor(128,0,128);
+  delay(t); // keep the color 1 second
+
+  
+  //Kırmızı  FF0000  255,0,0
+  setColor( 255,0,0);
+  delay(t); // keep the color 1 second
+
+  
+  //RosyBrown  BC8F8F  188.143.1430
+  setColor(188,143,1430);
+  delay(t); // keep the color 1 second
+
+  
+  //Kraliyet mavisi  4169E1  65.105.225
+  setColor( 65,105,225);
+  delay(t); // keep the color 1 second
+
+  
+  //Somon  FA8072  250.128.114
+  setColor(250,128,114);
+  delay(t); // keep the color 1 second
+
+  //SandyBrown  F4A460  244.164,96
+  setColor(244,164,96);
+  delay(t); // keep the color 1 second
+
+  //Deniz yeşili  2E8B57  46,139,87
+  setColor(46,139,87);
+  delay(t); // keep the color 1 second
+
+  
+  //Gökyüzü mavi  87CEEB  135.206.235
+  setColor(135,206,235);
+  delay(t); // keep the color 1 second
+
+  
+  //SlateBlue  6A5ACD  106,90,205
+  setColor( 106,90,205);
+  delay(t); // keep the color 1 second
+
+  
+  //Kar  FFFAFA  255.250.250255
+  setColor( 255,250,250);
+  delay(t); // keep the color 1 second
+
+  
+  //İlkbahar yesili  00FF7F  0,255,127
+  setColor( 0,255,127);
+  delay(t); // keep the color 1 second
+
+  
+  //Tan  D2B48C  210.180.140
+  setColor(210,180,140);
+  delay(t); // keep the color 1 second
+
+  
+  //Devedikeni  D8BFD8  216.191.216
+  setColor(216,191,216);
+  delay(t); // keep the color 1 second
+
+  
+  //Domates  FF6347  255,99,71
+  setColor(255,99,71);
+  delay(t); // keep the color 1 second
+
+  
+  //Turkuaz  40E0D0  64.224.208
+  setColor( 64,224,208);
+  delay(t); // keep the color 1 second
+
+  
+  //Menekşe  EE82EE  238.130.238
+  setColor(238,130,238);
+  delay(t); // keep the color 1 second
+
+  
+  //Buğday  F5DEB3  245.222.179
+  setColor( 245,222,179);
+  delay(t); // keep the color 1 second
+//
+ // Beyaz  FFFFFF  255.255.255
+  setColor( 255,255,255);
+  delay(t); // keep the color 1 second
+
+  //Sarı  FFFF00  255.255,0
+  setColor( 255,255,0);
+  delay(t); // keep the color 1 second
+
+  
+  //Sarı yeşil  9ACD32  154,205,50
+  setColor(154,205,50);
+  delay(t); // keep the color 1 second
+
+  
+  
+
+  
+  //
+}
+
+void setColor(int R, int G, int B) {
+  analogWrite(PIN_RED,   R);
+  analogWrite(PIN_GREEN, G);
+  analogWrite(PIN_BLUE,  B);
+  t = analogRead (A0);
+   t=  map(t,0,1023,200,9000);
+}
